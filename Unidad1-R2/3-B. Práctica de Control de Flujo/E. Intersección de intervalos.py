bf3dee70225764255f2e671735e23cac923af9cb
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-17 11:27:01 -0600

    Create E. Intersección de intervalos.py

---
+++ Unidad1-R2/3-B. Práctica de Control de Flujo/E. Intersección de intervalos.py
@@ -0,0 +1,9 @@
+def existe_interseccion(intervalo1_inicio, intervalo1_fin, intervalo2_inicio, intervalo2_fin):
+    if intervalo1_fin < intervalo2_inicio or intervalo2_fin < intervalo1_inicio:
+        return 0
+    else:
+        return 1
+entrada = input().split()
+intervalo1_inicio, intervalo1_fin, intervalo2_inicio, intervalo2_fin = map(int, entrada)
+resultado = existe_interseccion(intervalo1_inicio, intervalo1_fin, intervalo2_inicio, intervalo2_fin)
+print(resultado)
