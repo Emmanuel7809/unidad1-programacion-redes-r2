e3c55a13f6a38b8f9528796cce76b48a61ed652a
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-17 16:13:18 -0600

    Create E. Los Números de Missa.py

---
+++ Unidad1-R2/5-B. Práctica de Funciones/E. Los Números de Missa.py
@@ -0,0 +1,14 @@
+def grado_numero_missa(numero):
+    def suma_digitos_potencia(numero, potencia):
+        return sum(int(digito)**potencia for digito in str(numero))
+
+    for potencia in range(1, 10): 
+        suma = suma_digitos_potencia(numero, potencia)
+        if suma == numero:
+            return potencia
+        if suma > numero:
+            break
+    return -1
+entrada = int(input())
+resultado = grado_numero_missa(entrada)
+print(resultado)
