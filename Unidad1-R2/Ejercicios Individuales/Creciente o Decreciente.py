bdee79514c89eee40ec7d94e57b58bdd4ed10409
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 14:49:37 -0600

    Create Creciente o Decreciente.py

---
+++ Unidad1-R2/Ejercicios Individuales/Creciente o Decreciente.py
@@ -0,0 +1,6 @@
+N = int(input())
+conjunto = list(map(int, input().split()))
+creciente = sorted(conjunto)
+decreciente = sorted(conjunto, reverse=True)
+print(" ".join(map(str, creciente)))
+print(" ".join(map(str, decreciente)))
