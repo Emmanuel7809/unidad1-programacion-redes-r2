8b77dc6b935373e0358e034e071de0d6fd29e29c
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:53:34 -0600

    Update F. Multiplos dentro de un intervalo.py

--- Unidad1-R2/3-B. Práctica de Control de Flujo/F. Multiplos dentro de un intervalo.py
+++ Unidad1-R2/3-B. Práctica de Control de Flujo/F. Multiplos dentro de un intervalo.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 def imprimir_multiplos(a, b):
     for i in range(a, b + 1, a):
         print(i, end=" ")
