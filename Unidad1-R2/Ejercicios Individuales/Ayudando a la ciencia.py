2e67f232a2c3660a635ef4e990f40d414abec65e
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:40:49 -0600

    Update Ayudando a la ciencia.py

--- Unidad1-R2/Ejercicios Individuales/Ayudando a la ciencia.py
+++ Unidad1-R2/Ejercicios Individuales/Ayudando a la ciencia.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 def CelsiusaFarenheit(celsius):
     farenheit = (celsius * 9/5) + 32
     return farenheit
