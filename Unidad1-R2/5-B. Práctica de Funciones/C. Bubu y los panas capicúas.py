7106077d9413973651bce0fcaa75196ac2f82f4e
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:54:40 -0600

    Update C. Bubu y los panas capicúas.py

--- Unidad1-R2/5-B. Práctica de Funciones/C. Bubu y los panas capicúas.py
+++ Unidad1-R2/5-B. Práctica de Funciones/C. Bubu y los panas capicúas.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 def es_capicua(numero):
     return str(numero) == str(numero)[::-1]
 def obtener_capicua(num):
