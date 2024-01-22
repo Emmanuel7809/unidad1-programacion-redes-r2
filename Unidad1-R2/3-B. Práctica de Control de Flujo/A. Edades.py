f81d3b6b0d55325f8d9fae8a3637dcf4ff76f2d0
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:51:52 -0600

    Update A. Edades.py

--- Unidad1-R2/3-B. Práctica de Control de Flujo/A. Edades.py
+++ Unidad1-R2/3-B. Práctica de Control de Flujo/A. Edades.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 from collections import Counter
 num_alumnos = int(input())
 edades = list(map(int, input().split()))
