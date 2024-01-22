77b2e65ef992397d8175589ac75a584dc4124b9e
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 19:14:21 -0600

    Create Menor y mayor.py

---
+++ Unidad1-R2/Ejercicios Individuales/Menor y mayor.py
@@ -0,0 +1,10 @@
+a, b, c, d = map(int, input().split())
+suma1 = a + b
+suma2 = a + c
+suma3 = a + d
+suma4 = b + c
+suma5 = b + d
+suma6 = c + d
+menor_suma = min(suma1, suma2, suma3, suma4, suma5, suma6)
+mayor_suma = max(suma1, suma2, suma3, suma4, suma5, suma6)
+print(menor_suma, mayor_suma)
