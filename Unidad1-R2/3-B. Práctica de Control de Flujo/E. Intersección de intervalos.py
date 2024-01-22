6c8c5b458e9097a908fe6eb19f48661bceb4652f
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:53:18 -0600

    Update E. Intersección de intervalos.py

--- Unidad1-R2/3-B. Práctica de Control de Flujo/E. Intersección de intervalos.py
+++ Unidad1-R2/3-B. Práctica de Control de Flujo/E. Intersección de intervalos.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 def existe_interseccion(intervalo1_inicio, intervalo1_fin, intervalo2_inicio, intervalo2_fin):
     if intervalo1_fin < intervalo2_inicio or intervalo2_fin < intervalo1_inicio:
         return 0
