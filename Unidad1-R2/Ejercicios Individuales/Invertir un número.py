373cdc5cc22cd6b13cd8d3375edf45830dc4af39
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:47:45 -0600

    Update Invertir un número.py

--- Unidad1-R2/Ejercicios Individuales/Invertir un número.py
+++ Unidad1-R2/Ejercicios Individuales/Invertir un número.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 entrada = input()
 if len(entrada) < 2:
     print("Por favor, ingrese un número con al menos dos dígitos.")
