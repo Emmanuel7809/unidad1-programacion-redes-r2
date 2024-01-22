4dc053feb890afd7785e4058ae4ce79b1b28e2ac
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:51:35 -0600

    Update B. ¿Bubulu-cuántos instructores hay?.py

--- Unidad1-R2/2-B. Práctica de Colecciones de Datos/B. ¿Bubulu-cuántos instructores hay?.py
+++ Unidad1-R2/2-B. Práctica de Colecciones de Datos/B. ¿Bubulu-cuántos instructores hay?.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 instructores = input().split()
 cantidad_instructores = len(instructores)
 print(cantidad_instructores)
