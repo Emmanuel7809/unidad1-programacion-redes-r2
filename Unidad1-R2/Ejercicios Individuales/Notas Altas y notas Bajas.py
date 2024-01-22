8c3a58fa7f38e2e726d622b3eb483ffd80ea3da9
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:48:38 -0600

    Update Notas Altas y notas Bajas.py

--- Unidad1-R2/Ejercicios Individuales/Notas Altas y notas Bajas.py
+++ Unidad1-R2/Ejercicios Individuales/Notas Altas y notas Bajas.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 def calcular_promedio(notas):
     return round(sum(notas) / len(notas), 2)
 n = int(input())
