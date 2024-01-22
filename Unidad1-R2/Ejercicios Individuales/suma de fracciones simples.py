bc1410bb469bbc62139fa0b2f77fe17ea2a586b8
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:49:34 -0600

    Update and rename suma de fracciones simples,py to suma de fracciones simples.py

--- Unidad1-R2/Ejercicios Individuales/suma de fracciones simples.py
+++ Unidad1-R2/Ejercicios Individuales/suma de fracciones simples.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 num1, den1, num2, den2 = [int(input()) for _ in range(4)]
 num_suma = num1 * den2 + num2 * den1
 den_suma = den1 * den2
