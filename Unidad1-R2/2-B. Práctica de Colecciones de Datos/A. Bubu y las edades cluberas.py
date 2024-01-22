bbc55aa1f0bd2d6f623caa8d7fec288b85c5112b
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:51:22 -0600

    Update A. Bubu y las edades cluberas.py

--- Unidad1-R2/2-B. Práctica de Colecciones de Datos/A. Bubu y las edades cluberas.py
+++ Unidad1-R2/2-B. Práctica de Colecciones de Datos/A. Bubu y las edades cluberas.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 edades = input().split()
 edades_unicas = sorted(set(edades), key=int, reverse=True)
 print(edades_unicas)
