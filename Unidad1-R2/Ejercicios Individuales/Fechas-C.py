104f352ab46696fffb2abbd5728963f2810cb7de
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:32:54 -0600

    Create Fechas-C.py

---
+++ Unidad1-R2/Ejercicios Individuales/Fechas-C.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+def calcular_diferencia_entre_fechas(a, b, c, p, q, r):
+    fecha_inicial = datetime(c, b, a)
+    fecha_final = datetime(r, q, p)
+    diferencia = fecha_final - fecha_inicial
+    return abs(diferencia.days)
+fecha1 = input("Ingrese la primera fecha (a b c): ").split()
+fecha2 = input("Ingrese la segunda fecha (p q r): ").split()
+a, b, c = map(int, fecha1)
+p, q, r = map(int, fecha2)
+diferencia_dias = calcular_diferencia_entre_fechas(a, b, c, p, q, r)
+print(diferencia_dias)
