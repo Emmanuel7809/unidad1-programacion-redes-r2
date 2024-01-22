ee7ff81a4893325117c4581552c67a449e4efca4
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:50:49 -0600

    Update D. Promedios.py

--- Unidad1-R2/1-B. Práctica de Variables/D. Promedios.py
+++ Unidad1-R2/1-B. Práctica de Variables/D. Promedios.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 calificaciones = sum(map(int, input().split()))
 promedio = calificaciones / 5
 print(int(promedio))
