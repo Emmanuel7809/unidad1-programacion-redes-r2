2bb6cef8694c1a15cdd33730c3cbfbb8331f27df
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:53:02 -0600

    Update D. Ayuda a una Tienda.py

--- Unidad1-R2/3-B. Práctica de Control de Flujo/D. Ayuda a una Tienda.py
+++ Unidad1-R2/3-B. Práctica de Control de Flujo/D. Ayuda a una Tienda.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 def calcular_descuento(monto):
     if monto < 500:
         return 0
@@ -9,22 +11,18 @@ def calcular_descuento(monto):
         return 0.18
     else:
         return 0.25
-
 def calcular_monto_a_pagar(monto_compra):
     descuento = calcular_descuento(monto_compra)
     monto_a_pagar = monto_compra * (1 - descuento)
     return monto_a_pagar
-
 if __name__ == "__main__":
     montos_compra = []
-
     while True:
         try:
             monto = float(input())
             montos_compra.append(monto)
         except EOFError:
             break
-
     for monto in montos_compra:
         monto_a_pagar = calcular_monto_a_pagar(monto)
         print("{:.2f}".format(monto_a_pagar))
