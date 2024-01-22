c127f647b613498c53ff9dffb73837e3c92c79e8
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:50:38 -0600

    Update C. Suma simple.py

--- Unidad1-R2/1-B. Práctica de Variables/C. Suma simple.py
+++ Unidad1-R2/1-B. Práctica de Variables/C. Suma simple.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 numeros = input().split()
 numero1, numero2 = map(int, numeros)
 print(numero1 + numero2)
