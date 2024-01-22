865cb87dc1600f2055e6e7e0c5cf5d11e6172551
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:54:16 -0600

    Update A. Future Squads en Fornais Fácil.py

--- Unidad1-R2/5-B. Práctica de Funciones/A. Future Squads en Fornais Fácil.py
+++ Unidad1-R2/5-B. Práctica de Funciones/A. Future Squads en Fornais Fácil.py
@@ -1,12 +1,12 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n - 1)
-
 def combinaciones(n, r):
     return factorial(n) // (factorial(r) * factorial(n - r))
-
 nombres = input().split()
 nombres = [nombre for nombre in nombres if nombre not in ["Ricardo", "Mirón"]]
 num_personas = len(nombres)
