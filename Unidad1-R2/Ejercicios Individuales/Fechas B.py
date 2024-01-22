20d9f7e6b8723fc9f8ca54abb972e9f1d4835dbc
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2024-01-21 21:43:58 -0600

    Update Fechas B.py

--- Unidad1-R2/Ejercicios Individuales/Fechas B.py
+++ Unidad1-R2/Ejercicios Individuales/Fechas B.py
@@ -1,3 +1,5 @@
+### Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes Unidad 1 Recuperacion 2
 def sumar_dias(a, b, c, n):
     from datetime import datetime, timedelta
     fecha_inicial = datetime(c, b, a)
